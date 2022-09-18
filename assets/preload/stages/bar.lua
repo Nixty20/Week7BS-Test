ad66cfb9d3004eb63a9d82d74cec67c0d2b38a8b
Nixty20 <100542815+Nixty20@users.noreply.github.com>
2022-09-18 20:55:49 -0300

    Add files via upload

--- assets/preload/stages/bar.lua
+++ assets/preload/stages/bar.lua
@@ -1,24 +1,27 @@
 function onCreate()
+	-- background shit
+	makeLuaSprite('sky', 'week7/sky', -400,-400);
+	setLuaSpriteScrollFactor('sky', 0, 0);
+	scaleObject('ground', 15, 1.5)
 
-     makeLuaSprite('theSky','sky',-25,-400)
-     addLuaSprite('theSky',false)
-     SetLuaSpriteScrollFactor('thesky',0.2,0.2);
+	makeLuaSprite('ground', 'week7/ground', -420,-150);
+	setLuaSpriteScrollFactor('bar', 1, 1);
+	scaleObject('ground', 1.15, 1.15);
 
-     makeLuaSprite('theBar','bar',-300,600)
-     addLuaSprite(,theBar',false)
-     SerLuaSpriteScrollFactor('theBar',0.5,0.8);
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+		makeLuaSprite('bar', 'week7/bar', -300,-20);
+		setLuaSpriteScrollFactor('bar', 0.2, 0.2);
+		scaleObject('bar', 1.2, 1.2);
+		
+		
+		
+	end
 
-     close(true)
-end
-
-function onBeatHit(...)--for every beat
-   -- body
-end
-
-function onStepHit(...)--for every step
-   -- body
-end
-
-function onUptade(...)
-   -- body
+	addLuaSprite('sky', false);
+	addLuaSprite('bar', false);
+	
+	addLuaSprite('ground', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
